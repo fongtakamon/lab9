f672f8246681fe9296750b526080b2677e8a1516
fongtakamon <noofongka@gmail.com>
2022-12-23 17:00:37 +0700

    finish

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,7 +3,30 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double Grade)
+{
+  if (Grade > 90)
+ {
+  return 'A';
+ }
+ else if (Grade > 75)
+ {
+  return 'B';
+ }
+ else if (Grade > 60)
+ {
+ return 'C';
+ }
+ else if (Grade > 45)
+ {
+ return 'D';
+ }
+ else
+ {
+ return 'F';
+ }
+ return 0;
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +40,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,44 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream> 
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 25){
+        int height;
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100){
+            cout << "Your character = " << "Chopper" << endl;
+            return 0;
+        }else if (height < 180){
+            cout << "Your character = " << "Usopp" << endl;
+            return 0;
+        }
+        int bounty;
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if (bounty > 1100000000){
+            cout << "Your character = " << "Zoro" << endl;
+            return 0;
+        }else {
+            cout << "Your character = " << "Sanji" << endl;
+            return 0;
+        }
+    }else if (age <=60){
+        int bounty;
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if (bounty > 500000000){
+            cout << "Your character = " << "Jinbe" << endl;
+            return 0;
+        }else {
+            cout << "Your character = " << "Franky" << endl;
+            return 0;
+        }
+        return 0;
+    }
+    cout << "Your character = " << "Brook" << endl;
+    return 0;
+}
